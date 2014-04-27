38f1776811699d18a9b3ca2cd0ace49f940c6e1a
xiaolang <xiaolang@foxmail.com>
2014-04-27 09:59:50 +0800

    day2 input

---
+++ day2/input2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+name = raw_input("name:")
+age = raw_input("age:")
+
+print "hello",name,'\n'
+print "ni",age,"sui!"
+
+
+
---
+++ day2/input3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+this = 2014
+name = raw_input("name:")
+age = int(raw_input("age:"))
+
+print "hello",name,'\n'
+print "ni",age,"sui!","\n"
+print "chu sheng:",this - age
+
+
+
